18ab49c89d8a29b8bc5dbc5c9adb5d967956bf92
Nikita Agupov <nikita.agupov@arrival.services>
2019-01-23 11:05:07 +0300

    coreui-24 common: increase limit size request json

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(async (request, response, next) => {
     next();
 });
 
-app.use(express.json());
+app.use(express.json({limit: '10mb', extended: true}));
 
 app.get('/', (req, res) => res.send('Hi! This is screenshoter service!'));
 
