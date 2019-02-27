a61b34eb81f1dcb4bfda2890a36cba2878fb4e95
Nikita Sergeev <nikita.sergeev@arrival.services>
2019-02-27 18:47:07 +0300

    COREUI-210: added scroll handler

---
+++ common/enums/emulate-action-type.enum.js
@@ -0,0 +1,7 @@
+const EmulateActionType = {
+    FOCUS: 'arrival-emulated-action-focus',
+    HOVER: 'arrival-emulated-action-hover',
+    SCROLL_Y: 'arrival-emulated-action-scroll-y'
+};
+
+module.exports = { EmulateActionType };
