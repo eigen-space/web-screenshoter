81f70ff1fbdc19dcd6df6087745a2c575d1ff9de
Nikita Agupov <nktrsk@gmail.com>
2019-04-04 20:19:01 +0300

    COREUI-289 common: update jenkins pipeline

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,3 +5,6 @@ cd /opt/services/screenshoter/
 yarn
 nohup node ./server.js > screenshoter.out &
 echo 'started...'
+
+cp /opt/services/screenshoter/config/nginx/sites-available/* /etc/nginx/sites-available
+sudo service nginx reload
