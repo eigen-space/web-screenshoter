a367f4e098b2d13244a08e1679d057fe7dbc2fd9
Nikita Agupov <nktrsk@gmail.com>
2019-04-07 21:55:29 +0300

    COREUI-289 common: update pipeline

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-chmod -R 755 /opt/services/screenshoter
 cd /opt/services/screenshoter/dist
 yarn
+chmod -R 755 /opt/services/screenshoter
 nohup node ./server.js > screenshoter.out &
 echo 'started...'
 
